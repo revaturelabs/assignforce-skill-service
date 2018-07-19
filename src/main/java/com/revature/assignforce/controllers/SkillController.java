403101f1fefe2d8a7a9a80c3a4710cdc1443c473
Jay Feldman <jayfman@gmail.com>
2018-07-19 11:32:22 -0400

    adding cross origin

--- src/main/java/com/revature/assignforce/controllers/SkillController.java
+++ src/main/java/com/revature/assignforce/controllers/SkillController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import com.revature.assignforce.containers.SkillsArray;
 import com.revature.assignforce.services.SkillService;
 
 @RestController
+@CrossOrigin
 public class SkillController {
 
 	@Autowired
