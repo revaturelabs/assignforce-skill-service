2369b21d8a83c3167572a7c2800921cb90d542df
Oliver Phi <istolethisemail.phi@gmail.com>
2018-07-20 13:01:54 -0400

    This one was exceptionally smelly.

--- src/main/java/com/revature/assignforce/SkillServiceApplication.java
+++ src/main/java/com/revature/assignforce/SkillServiceApplication.java
@@ -1,6 +1,5 @@
 package com.revature.assignforce;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -11,8 +10,6 @@ public class SkillServiceApplication {
 
 	public static void main(String[] args) throws Exception {
 		new SpringApplicationBuilder(SkillServiceApplication.class).run(args);
-
-//		SpringApplication.run(SkillServiceApplication.class, args);
 	}
 
 }
--- src/main/java/com/revature/assignforce/beans/Skill.java
+++ src/main/java/com/revature/assignforce/beans/Skill.java
@@ -1,16 +1,10 @@
 package com.revature.assignforce.beans;
 
-import java.util.Set;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
