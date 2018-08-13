094a9cb9d852de211dc6a75fb3450732a953c3c0
August Duet <august.duet@revature.com>
2018-08-13 12:31:09 -0400

    [debug]

--- src/main/java/com/revature/assignforce/messaging/messenger/SkillMessenger.java
+++ src/main/java/com/revature/assignforce/messaging/messenger/SkillMessenger.java
@@ -1,7 +1,5 @@
 package com.revature.assignforce.messaging.messenger; 
 
-import javax.inject.Inject;
-
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
