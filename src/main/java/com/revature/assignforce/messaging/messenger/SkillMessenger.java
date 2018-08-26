105918e7d0feb5bc098f63e056a987e4ffc71129
August Duet <august.duet@revature.com>
2018-08-25 22:59:37 -0400

    updating messaging for skill deactivation

--- src/main/java/com/revature/assignforce/messaging/messenger/SkillMessenger.java
+++ src/main/java/com/revature/assignforce/messaging/messenger/SkillMessenger.java
@@ -1,9 +1,9 @@
 package com.revature.assignforce.messaging.messenger; 
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import javax.inject.Inject;
@@ -12,7 +12,8 @@ import javax.inject.Inject;
 @Component
 public class SkillMessenger {
 
-	
+	Logger logger = LoggerFactory.getLogger(SkillMessenger.class);
+
 	private final RabbitTemplate rabbitTemplate;
 
 	@Value("${spring.rabbitmq.exchange:assignforce}")
@@ -21,13 +22,23 @@ public class SkillMessenger {
 	@Value("${spring.rabbitmq.skill-routing-delete:assignforce.skill.delete")
 	private String routingKey;
 
+	@Value("${spring.rabbitmq.skill-routing-deactivate:assignforce.skill.deactivate")
+	private String deactivateKey;
+
 	@Inject
 	public SkillMessenger(RabbitTemplate rabbitTemplate) {
 		super();
+		logger.info("Setting rabbit template to configured template");
 		this.rabbitTemplate = rabbitTemplate;
 	}
 
 	public void sendDeletionMessage(int id) {
+		logger.info("Sending deletion message for skill " + id);
 		rabbitTemplate.convertAndSend(exchange, routingKey, id);
+	}
+
+	public void sendDeactivateMessage(int id) {
+		logger.info("Sending deactivation message for skill " + id);
+		rabbitTemplate.convertAndSend(exchange, deactivateKey, id);
 	}
 }
