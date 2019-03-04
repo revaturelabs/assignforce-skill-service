45d5740206f83f3ba24bdeedeac1d5abd5a99410
August Duet <august.duet@revature.com>
2019-03-04 15:45:36 -0500

    setting up new pipeline and deployment enviro

--- src/main/java/com/revature/assignforce/SkillServiceApplication.java
+++ src/main/java/com/revature/assignforce/SkillServiceApplication.java
@@ -1,6 +1,5 @@
 package com.revature.assignforce;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
